89a81b52aedf8ee879172e69b064c430cf36c445
Craig Myles <cr@igmyles.com>
2018-10-19 09:31:22 +1100

    Added react app

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  state = {
+    timestamp: null
+  };
+
+  async componentDidMount() {
+    const response = await fetch("/api/timestamp");
+    const timestamp = await response.json();
+    console.log(timestamp);
+    this.setState({ timestamp });
+  }
+
+  render() {
+    const { timestamp } = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        {timestamp && <p>{timestamp}</p>}
+      </div>
+    );
+  }
+}
+
+export default App;
