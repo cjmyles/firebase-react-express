1dca99dc75d2da8f739ccc94d8bdc270c25d13c5
Craig Myles <cr@igmyles.com>
2018-12-06 11:34:17 +1100

    Working Facebook login
    Getting Facebook access token client side and passing to server to sign in user to Firebase before storing session cookie

--- functions/routes.js
+++ functions/routes.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const passport = require('./passport');
 const config = require('./config');
-const { admin } = require('./firebase');
+const { firebase, admin } = require('./firebase');
+
+// Firebase strips out client side cookies, except `__session`
+const SESSION_KEY = '__session';
 
 const router = express.Router();
 
+/**
+ * Middleware that ensures a user is authenticated. We use a Firebase method to
+ * decode a user from a session token and append this to the `req` object
+ * for the next middleware.
+ */
 const ensureAuthenticated = async (req, res, next) => {
   try {
-    const sessionCookie = req.cookies.session || '';
+    const sessionCookie = req.cookies[SESSION_KEY] || '';
+    console.log(sessionCookie);
     // Verify the session cookie. In this case an additional check is added to detect
     // if the user's Firebase session was revoked, user deleted/disabled, etc.
     const decodedClaims = await admin
@@ -16,15 +25,25 @@ const ensureAuthenticated = async (req, res, next) => {
     req.user = decodedClaims;
     return next();
   } catch (error) {
-    console.error(error);
+    // console.error(error);
     return res.status(401).send();
   }
 };
 
+/**
+ * Simple API response.
+ */
 router.get('/api/message', (req, res) => {
   res.send({ message: 'Hello world!' });
 });
 
+/**
+ * Deprecated: This requires a redirect from client->server->client which sets the session cookie, but future fetch requests cannot access the session.
+ *
+ * Facebook login entry point. The PassportJS strategy defined in `./passport.js`
+ * will attempt to log the user in and redirect to the callback URL specified there
+ * and referenced below.
+ */
 router.get(
   '/api/facebook',
   passport.authenticate('facebook', {
@@ -32,52 +51,136 @@ router.get(
   })
 );
 
+/**
+ * Deprecated: This requires a redirect from client->server->client which sets the session cookie, but future fetch requests cannot access the session.
+ *
+ * PassportJS Facebook callback. Create a session cookie and store this on the server.
+ */
 router.get('/api/facebook/callback', (req, res) =>
   passport.authenticate('facebook', async (error, user, info) => {
-    if (error) {
-      res.send({ error });
-    } else {
-      // Get the auth token
-      const token = await user.getIdToken();
-      console.info('Firebase auth token acquired');
-      // Set session expiration to 5 days.
-      const expiresIn = 60 * 60 * 24 * 5 * 1000;
-      // Create the session cookie. This will also verify the ID token in the process.
-      // The session cookie will have the same claims as the ID token.
-      // To only allow session cookie setting on recent sign-in, auth_time in ID token
-      // can be checked to ensure user was recently signed in before creating a session cookie.
-      const cookie = await admin
-        .auth()
-        .createSessionCookie(token, { expiresIn });
-      console.info('Cookie generated');
-      // Set cookie policy for session cookie.
-      const options = {
-        maxAge: expiresIn,
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-      };
-      res.cookie('session', cookie, options);
-      res.redirect(config.get('facebook.loginSuccessURL'));
+    try {
+      if (error) {
+        res.send({ error });
+      } else {
+        // Get the auth token
+        const token = await user.getIdToken();
+        console.info('Firebase auth token acquired');
+        // Set session expiration to 5 days.
+        const expiresIn = 60 * 60 * 24 * 5 * 1000;
+        // Create the session cookie. This will also verify the ID token in the process.
+        // The session cookie will have the same claims as the ID token.
+        // To only allow session cookie setting on recent sign-in, auth_time in ID token
+        // can be checked to ensure user was recently signed in before creating a session cookie.
+        const cookie = await admin
+          .auth()
+          .createSessionCookie(token, { expiresIn });
+        console.info('Cookie generated');
+        // Set cookie policy for session cookie.
+        const options = {
+          maxAge: expiresIn,
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production',
+        };
+        res.cookie(SESSION_KEY, cookie, options);
+        // res.redirect(config.get('facebook.loginSuccessURL'));
+        res.send({ [SESSION_KEY]: cookie });
+      }
+    } catch (err) {
+      res.send({ error: err });
     }
   })(req, res)
 );
 
+/**
+ * Sign a Facebook user into Firebase auth and create a session cookie.
+ */
+router.post('/api/facebook', async (req, res) => {
+  try {
+    const credential = firebase.auth.FacebookAuthProvider.credential(
+      req.body.accessToken
+    );
+    console.info('Firebase credential acquired');
+    // Sign in with credential from the Facebook user.
+    const result = await firebase
+      .auth()
+      .signInAndRetrieveDataWithCredential(credential);
+    console.info('Firebase user authenticated');
+    // Get the auth token
+    const token = await result.user.getIdToken();
+    console.info('Firebase auth token acquired');
+    // Set session expiration to 5 days.
+    const expiresIn = 60 * 60 * 24 * 5 * 1000;
+    // Create the session cookie. This will also verify the ID token in the process.
+    // The session cookie will have the same claims as the ID token.
+    // To only allow session cookie setting on recent sign-in, auth_time in ID token
+    // can be checked to ensure user was recently signed in before creating a session cookie.
+    const cookie = await admin.auth().createSessionCookie(token, { expiresIn });
+    console.info('Cookie generated');
+    // Set cookie policy for session cookie.
+    const options = {
+      maxAge: expiresIn,
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+    };
+    res.cookie(SESSION_KEY, cookie, options);
+    res.status(204).send();
+  } catch (error) {
+    res.send({ error });
+  }
+});
+
+/**
+ * Get a user's profile (this is based on Firebase auth rather than
+ * the direct Facebook profile, but still contains Facebook data)
+ */
 router.get('/api/profile', ensureAuthenticated, (req, res) => {
   res.send(req.user);
 });
 
+/**
+ * Log a user out.
+ */
 router.get('/api/logout', (req, res) => {
   req.logout();
-  res.clearCookie('session');
-  res.redirect(config.get('facebook.loginSuccessURL'));
+  res.clearCookie(SESSION_KEY);
+  res.status(204).send();
 });
 
+/**
+ * Return some debug data.
+ */
 router.get('/api/debug', (req, res) => {
   res.send({
     env: process.env.NODE_ENV,
     secure: process.env.NODE_ENV === 'production',
     cookies: req.cookies,
     headers: req.headers,
+  });
+});
+
+/**
+ * Set a test cookie.
+ * Note: Firebase strips all non `__session` cookies from the client
+ */
+router.post('/api/cookie', (req, res) => {
+  // Set session expiration to 5 days.
+  const expiresIn = 60 * 60 * 24 * 5 * 1000;
+  const options = {
+    maxAge: expiresIn,
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+  };
+  res.cookie(SESSION_KEY, 'test1234', options);
+  res.send({ status: 'success' });
+});
+
+/**
+ * Retrieve the test cookie.
+ * Note: Firebase strips all non `__session` cookies from the client
+ */
+router.get('/api/cookie', (req, res) => {
+  res.send({
+    SESSION_KEY: req.cookies[SESSION_KEY],
   });
 });
 
--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React, { Component } from 'react';
+import FacebookLogin from 'react-facebook-login';
 import logo from './logo.svg';
 import './App.css';
 
-const apiUrl =
-  process.env.NODE_ENV === 'production'
-    ? 'https://us-central1-node-test-f2063.cloudfunctions.net/app/api'
-    : 'http://localhost:5001/node-test-f2063/us-central1/app/api';
-
-const facebookLoginURL = apiUrl + '/facebook';
-const facebookLogoutURL = apiUrl + '/logout';
+const APP_ID = '243883486171883';
 
 class App extends Component {
   state = {
@@ -24,7 +19,7 @@ class App extends Component {
     } catch (error) {}
   }
 
-  async facebookProfile() {
+  async getFacebookProfile() {
     const response = await fetch('/api/profile');
     try {
       const profile = await response.json();
@@ -40,11 +35,59 @@ class App extends Component {
     } catch (error) {}
   }
 
+  async setCookie() {
+    const response = await fetch('/api/cookie', { method: 'POST' });
+    try {
+      const json = await response.json();
+      console.log('Cookie set:', json.status);
+    } catch (error) {}
+  }
+
+  async getCookie() {
+    const response = await fetch('/api/cookie');
+    try {
+      const cookie = await response.json();
+      console.log('Cookie get:', cookie);
+    } catch (error) {}
+  }
+
   async componentDidMount() {
     this.testAPI();
-    this.facebookProfile();
+    this.getFacebookProfile();
+    // await this.setCookie();
+    // this.getCookie();
     this.debug();
   }
+
+  handleLoginClick = event => {
+    console.log(event);
+  };
+
+  handleFacebookCallback = async fbResponse => {
+    try {
+      await fetch('/api/facebook', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ accessToken: fbResponse.accessToken }),
+      });
+      this.getFacebookProfile();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  handleLogout = async () => {
+    try {
+      await fetch('/api/logout');
+      this.setState({ profile: null });
+      console.log('Logged out!');
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   render() {
     const { message, profile } = this.state;
@@ -59,20 +102,26 @@ class App extends Component {
             {profile && (
               <li>
                 Facebook Profile:{' '}
-                <div>
-                  <pre>{JSON.stringify(profile, null, 2)}</pre>
-                </div>
+                <pre style={{ fontSize: 11 }}>
+                  {JSON.stringify(profile, null, 2)}
+                </pre>
               </li>
             )}
           </ul>
           {profile ? (
-            <a className="App-link" href={facebookLogoutURL}>
-              Logout
-            </a>
+            <div className="App-facebook">
+              <button onClick={this.handleLogout}>Logout</button>
+            </div>
           ) : (
-            <a className="App-link" href={facebookLoginURL}>
-              Facebook Login
-            </a>
+            <div className="App-facebook">
+              <FacebookLogin
+                appId={APP_ID}
+                autoLoad={false}
+                fields="name,email,picture"
+                //onClick={this.handleLoginClick}
+                callback={this.handleFacebookCallback}
+              />
+            </div>
           )}
         </header>
       </div>
